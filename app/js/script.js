7cea36d8d2a4f8b8916155bbfd7d43926403ec1c
ar78ilnik <arakchiev.ilya@yandex.ru>
2021-10-31 15:14:21 +0300

    Header all

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,20 @@
     const mainBlock = document.querySelector('.header__menu'),
-        svgImg = mainBlock.querySelector('.header__icon'),
-        menuMain = document.querySelector('.menu_main');
-        mainBlock.addEventListener('click', function() {
-            console.log('1');
+        svgImg = mainBlock.querySelector('.header__svg'),
+        menuMain = document.querySelector('.menu_main'),
+        menuBack = document.querySelector('.menu__back'),
+        menuClose = document.querySelector('.menu__close');
+    mainBlock.addEventListener('click', function() {
         let expanded = mainBlock.getAttribute('aria-expanded') === 'true' || false;
         mainBlock.setAttribute('aria-expanded', !expanded);
-        svgImg.classList.toggle('rotated');
+        svgImg.classList.toggle('header__svg_rotated');
         menuMain.classList.toggle('menu_opened');
+        menuBack.classList.toggle('menu__back_opened');
+    });
+
+    menuClose.addEventListener('click', function() {
+        let expanded = mainBlock.getAttribute('aria-expanded') === 'true' || false;
+        menuMain.classList.toggle('menu_opened');
+        menuBack.classList.toggle('menu__back_opened');
+        mainBlock.setAttribute('aria-expanded', !expanded);
+        svgImg.classList.toggle('header__svg_rotated');
     });
